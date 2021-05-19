eff00033935294b39978aaeb06044597807d775b
Joe Lingyu <lingyu.zhou@biogen.com>
2021-05-19 15:16:04 -0400

    fix test

--- tests/scheduler.spec.ts
+++ tests/scheduler.spec.ts
@@ -39,7 +39,7 @@ describe('OfflineScheduler', () => {
     expect(log).toBeCalledTimes(0);
   });
 
-  const scheduleFunction = {
+  const enabledScheduleFunction = {
     'schedule-function': {
       handler: 'src/functions/schedule-function.handler',
       events: [
@@ -47,6 +47,14 @@ describe('OfflineScheduler', () => {
           schedule: {
             name: '1-minute',
             rate: 'rate(1 minute)',
+            input: { scheduler: '1-minute' },
+          },
+        },
+        {
+          schedule: {
+            name: '1-minute',
+            rate: 'rate(1 minute)',
+            enabled: true,
             input: { scheduler: '1-minute' },
           },
         },
@@ -60,14 +68,19 @@ describe('OfflineScheduler', () => {
     const scheduleJob = jest.spyOn(schedule, 'scheduleJob');
     const scheduler = new Scheduler({
       log,
-      functionProvider: () => scheduleFunction,
+      functionProvider: () => enabledScheduleFunction,
     });
 
     scheduler.scheduleEvents();
-    expect(log).toBeCalledWith(
+    expect(log).nthCalledWith(
+      1,
       'Scheduling [schedule-function] cron: [*/1 * * * *] input: {"scheduler":"1-minute"}'
     );
-    expect(scheduleJob).toBeCalledTimes(1);
+    expect(log).nthCalledWith(
+      2,
+      'Scheduling [schedule-function] cron: [*/1 * * * *] input: {"scheduler":"1-minute"}'
+    );
+    expect(scheduleJob).toBeCalledTimes(2);
   });
 
   const disabledScheduleFunction = {
@@ -78,7 +91,7 @@ describe('OfflineScheduler', () => {
           schedule: {
             name: '1-minute',
             rate: 'rate(1 minute)',
-            disabled: false,
+            enabled: false,
             input: { scheduler: '1-minute' },
           },
         },
@@ -96,11 +109,24 @@ describe('OfflineScheduler', () => {
     });
 
     scheduler.scheduleEvents();
-    expect(log).toBeCalledWith(
-      'Scheduling [schedule-function] cron: [*/1 * * * *] input: {"scheduler":"1-minute"}'
-    );
-    expect(scheduleJob).toBeCalledTimes(1);
+    expect(log).toBeCalledTimes(0);
   });
+
+  const scheduleFunction = {
+    'schedule-function': {
+      handler: 'src/functions/schedule-function.handler',
+      events: [
+        {
+          schedule: {
+            name: '1-minute',
+            rate: 'rate(1 minute)',
+            input: { scheduler: '1-minute' },
+          },
+        },
+      ],
+      name: 'my-service-dev-schedule-function',
+    },
+  };
 
   it('Should schedule job in standalone process when function with schedule provided', () => {
     const log = jest.fn();
